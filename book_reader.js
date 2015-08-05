8d4d391ec4395292865068f8a0839f9fccc124fe
bbouley <b.bouley@hotmail.co.uk>
2015-08-04 19:12:45 -0600

    completed book reader so it passes jasmine tests

--- book_reader.js
+++ book_reader.js
@@ -1,21 +1,38 @@
 function BookReader(book) {
-  // your code here
+  this.book = book;
+  this.currentPage = 0;
 }
 
 BookReader.prototype.nextPage = function() {
-  // your code here
+  if(this.currentPage < this.book.length-1){
+  this.currentPage += 1;
+  return this.book[this.currentPage];
+  } else {
+  return this.book[this.currentPage];
+  }
 };
 
 BookReader.prototype.previousPage = function() {
-  // your code here
+  if(this.currentPage > 1){
+  this.currentPage -= 1;
+  return this.book[this.currentPage];
+  } else  {
+    return this.book[0];
+  }
 };
 
 BookReader.prototype.pagesLeft = function() {
-  // your code here
+  pagesLeft = this.book.length - (this.currentPage+1);
+  return pagesLeft;
 };
 
 BookReader.prototype.encouragement = function() {
-  // your code here
+  pagesLeft = this.book.length - (this.currentPage+1);
+  if (pagesLeft === 2){
+    return 'Keep going, only 2 pages left after this one!';
+  } else if (pagesLeft === 1){
+    return 'Keep going, this book is good \'til the last drop!';
+  }
 };
 
 // DO NOT MODIFY BELOW THIS COMMENT:
