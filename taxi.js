2ed2f8298f42769aa89450e91041b4b68acc3c6c
bbouley <b.bouley@hotmail.co.uk>
2015-08-04 19:30:35 -0600

    passed all jasmine tests for taxi part

--- taxi.js
+++ taxi.js
@@ -1,12 +1,33 @@
 function Taxi(driverName, maxNumberOfPassengers) {
-  // your code here
+  this.driverName = driverName;
+  this.maxNumberOfPassengers = maxNumberOfPassengers;
+  this.passengers = [];
 }
 
 Taxi.prototype.addPassenger = function(passengerName) {
-  // your code here
+  if(this.passengers.length < this.maxNumberOfPassengers){
+  this.passengers.push(passengerName);
+ }
 };
 
 // your code here
+
+Taxi.prototype.passengerCount = function(){
+  return this.passengers.length;
+};
+
+Taxi.prototype.full = function(){
+  if (this.passengers.length === this.maxNumberOfPassengers){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Taxi.prototype.dropOffPassengers = function(){
+  this.passengers = [];
+  return this.passengers;
+};
 
 // DO NOT MODIFY BELOW THIS COMMENT:
 module.exports = Taxi;
